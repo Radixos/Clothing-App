8b65e255b11c795274a1eb023dedf5ceec37adb7
Radoslaw Zajdel <radoslaw.zajdel@inty.com>
2023-09-15 12:58:33 +0100

    Applied redux and reducer themes to categories

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,14 +1,16 @@
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
-import { CategoriesContext } from "../../contexts/categories.context";
+import { selectCategoriesMap } from "../../store/categories/category.selector";
+
 import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const CategoriesPreview = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
+    const categoriesMap = useSelector(selectCategoriesMap);
 
     return (
         <Fragment>
-            {
+        {
                 Object.keys(categoriesMap).map((title) => {
                     const products = categoriesMap[title];
                     return (
