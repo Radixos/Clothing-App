454ba84351b5a0cee23b2b5eafead67cb28fdae5
Radoslaw Zajdel <radoslaw.zajdel@inty.com>
2023-09-21 13:20:32 +0100

    Migrated cart context to Redux

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,15 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+
+    next(action);
+}
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
