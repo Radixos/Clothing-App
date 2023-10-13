82ce4ba1fbc500ce5828851571050dadc45bb543
Radoslaw Zajdel <radoslaw.zajdel@inty.com>
2023-10-13 13:25:54 +0100

    Implemented stripe and fixed relevant issues

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 import "./checkout.styles.scss"
 import CartItem from "../../components/cart-item/cart-item.component";
@@ -36,8 +37,9 @@ const Checkout = () => {
                 )
             }
             <span className="total">Total: £{cartTotal}</span>
+            <PaymentForm />
         </div>
-    )
-}
+    );
+};
 
 export default Checkout;
